23163c849b408f22b2ba221c67bf41919bbe2a62
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-12-11 08:55:44 +0200

    ui

---
+++ server/src/main/java/com/payrent/PayrentApplication.java
@@ -0,0 +1,11 @@
+package com.payrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrentApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PayrentApplication.class, args);
+    }
+}
